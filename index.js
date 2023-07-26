b7a9d4564c5b709d63bded3579069823fdcad742
SushantDahal <SushanDahal246@gmail.com>
2023-07-26 22:43:53 +0545

    completed the background blur image

---
+++ index.js
@@ -0,0 +1,22 @@
+const loadText = document.querySelector(".loading-text");
+const bg = document.querySelector(".back");
+
+let load = 0;
+
+let int = setInterval(Blurring, 30);
+
+function Blurring() {
+  load++;
+
+  if (load > 99) {
+    clearInterval(int);
+  }
+
+  loadText.innerHTML = `${load}%`;
+  loadText.style.opacity = scale(load, 0, 100, 1, 0);
+  bg.style.filter = `blur(${scale(load, 0, 100, 40, 0)}px)`;
+}
+
+const scale = (number, inMin, inMax, outMin, outMax) => {
+  return ((number - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
+};
